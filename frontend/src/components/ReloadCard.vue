dd65bf5c54ebee68f1e437dfa6feeb483de89c6c
Omar De Leo <omar.deleo@gmail.com>
2021-03-05 19:08:14 -0500

    Make code snippets read-only

--- frontend/src/components/ReloadCard.vue
+++ frontend/src/components/ReloadCard.vue
@@ -30,6 +30,7 @@
           class="my-editor"
           v-model="codeSnippet1"
           :highlight="highlighter"
+          :readonly="true"
         >
         </prism-editor>
       </div>
--- frontend/src/components/ThemeCard.vue
+++ frontend/src/components/ThemeCard.vue
@@ -30,6 +30,7 @@
           class="my-editor"
           v-model="codeSnippet2"
           :highlight="highlighter"
+          :readonly="true"
         >
         </prism-editor>
       </div>
@@ -45,13 +46,12 @@
 
 <script>
 import { PrismEditor } from 'vue-prism-editor';
-import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
+import 'vue-prism-editor/dist/prismeditor.min.css';
 
-// import highlighting library (you can use any library you want just return html string)
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
+import 'prismjs/themes/prism-tomorrow.css';
 
 export default {
   name: 'ThemeCard',
